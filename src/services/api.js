646c7b577c5ddcef8e04ccf8e1f743302de0e798
rafaeelnunes <rafaelnfsq@gmail.com>
2022-02-04 21:47:55 -0300

    updating api url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://my-wallet-back.herokuapp.com";
 
 function register(body) {
   return axios.post(`${BASE_URL}/register`, body);
